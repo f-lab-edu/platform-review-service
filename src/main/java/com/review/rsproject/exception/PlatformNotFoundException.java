9642604537c1c695a522d6e2c1217402350a7736
penguinhing <unknown134959@naver.com>
2024-04-28 20:40:29 +0900

    플랫폼 예외 추가

--- src/main/java/com/review/rsproject/exception/PlatformNotFoundException.java
+++ src/main/java/com/review/rsproject/exception/PlatformNotFoundException.java
@@ -1,2 +1,11 @@
-package com.review.rsproject.exception;public class PlatformNotFoundException {
+package com.review.rsproject.exception;
+
+public class PlatformNotFoundException extends RuntimeException{
+
+    public PlatformNotFoundException() {
+    }
+
+    public PlatformNotFoundException(String message) {
+        super(message);
+    }
 }
