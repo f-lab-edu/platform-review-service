65d4ea1394e76be940815e89fdb14c6acfac0e79
penguinhing <unknown134959@naver.com>
2024-04-28 20:42:36 +0900

    플랫폼 상태 수정 기능 구현

--- src/main/java/com/review/rsproject/dto/PlatformEditDto.java
+++ src/main/java/com/review/rsproject/dto/PlatformEditDto.java
@@ -1,2 +1,28 @@
-package com.review.rsproject.dto;public class PlatformEditDto {
+package com.review.rsproject.dto;
+
+import com.review.rsproject.type.PlatformStatus;
+import com.review.rsproject.validator.ValidEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlatformEditDto {
+
+
+    @NotNull
+    private Long id;
+
+    @Length(min = 10, max = 50)
+    private String description;
+
+    @ValidEnum(enumClass = PlatformStatus.class)
+    private PlatformStatus status;
+
 }
--- src/main/java/com/review/rsproject/type/PlatformStatus.java
+++ src/main/java/com/review/rsproject/type/PlatformStatus.java
@@ -1,2 +1,17 @@
-package com.review.rsproject.type;public enum PlatformStatus {
+package com.review.rsproject.type;
+
+public enum PlatformStatus {
+    WAIT("WAIT"),
+    ACCEPT("ACCEPT"),
+    DENY("DENY");
+
+    private final String status;
+
+    PlatformStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
