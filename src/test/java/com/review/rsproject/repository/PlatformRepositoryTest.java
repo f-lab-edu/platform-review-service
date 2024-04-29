7b2a6b0f94336e458459cbf33df8a0d923c593b3
penguinhing <unknown134959@naver.com>
2024-04-29 20:19:58 +0900

    피드백 반영

--- src/test/java/com/review/rsproject/repository/PlatformRepositoryTest.java
+++ src/test/java/com/review/rsproject/repository/PlatformRepositoryTest.java
@@ -1,16 +1,16 @@
 package com.review.rsproject.repository;
 
+import com.review.rsproject.common.ConstantValues;
 import com.review.rsproject.domain.Member;
 import com.review.rsproject.domain.Platform;
 import com.review.rsproject.type.MemberRole;
 import com.review.rsproject.type.PlatformStatus;
 
-import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -20,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -31,49 +32,85 @@ class PlatformRepositoryTest {
     @Autowired PlatformRepository platformRepository;
     @Autowired MemberRepository memberRepository;
 
+
     @Test
-    @DisplayName("플랫폼 상태 값에 따른 조회 테스트")
-    void findStatus() {
-
+    @DisplayName("플랫폼 대기 상태 조회")
+    void find_status_wait() {
         // given
-        Member member = new Member("test", "test", MemberRole.ROLE_USER);
-        memberRepository.save(member);
-
-        // 각 상태별 데이터를 15개씩 저장
-        for (PlatformStatus status : PlatformStatus.values()) {
-            for (int i = 1; i <= 15; ++i) {
-
-                Platform platform = new Platform(status.getStatus() + i , "https://tt.com", "test", member);
-                platform.changeInfo(null, status);
-                platformRepository.save(platform);
-            }
-        }
+        saveBulk(PlatformStatus.WAIT, 12);
+        Pageable pageRequest = PageRequest.of(0, ConstantValues.PAGE_SIZE);
 
         // when
-        Pageable pageRequest = PageRequest.of(0, 10);
-
-        Page<Platform> platform_wait = platformRepository.findByStatus(pageRequest, PlatformStatus.WAIT);
-
-        Page<Platform> platform_accept = platformRepository.findByStatus(pageRequest, PlatformStatus.ACCEPT);
-        Page<Platform> platform_deny = platformRepository.findByStatus(pageRequest, PlatformStatus.DENY);
-
-        Page<Platform> platform_all = platformRepository.findByStatus(pageRequest, null);
+        Page<Platform> platform = platformRepository.findByStatus(pageRequest, PlatformStatus.WAIT);
 
 
         // then
-        assertThat(platform_wait.getContent().size()).isEqualTo(10);
-        assertThat(platform_wait.getContent()).allSatisfy(platform -> assertThat(platform.getStatus()).isEqualTo(PlatformStatus.WAIT));
-
-        assertThat(platform_accept.getContent().size()).isEqualTo(10);
-        assertThat(platform_accept.getContent()).allSatisfy(platform -> assertThat(platform.getStatus()).isEqualTo(PlatformStatus.ACCEPT));
-
-        assertThat(platform_deny.getContent().size()).isEqualTo(10);
-        assertThat(platform_deny.getContent()).allSatisfy(platform -> assertThat(platform.getStatus()).isEqualTo(PlatformStatus.DENY));
-
-        assertThat(platform_all.getContent().size()).isEqualTo(10);
-
+        assertThat(platform.getContent().size()).isEqualTo(10);
+        assertThat(platform.getContent()).allSatisfy(p -> assertThat(p.getStatus()).isEqualTo(PlatformStatus.WAIT));
     }
 
+    @Test
+    @DisplayName("플랫폼 승인 상태 조회")
+    void find_status_accept() {
+        // given
+        saveBulk(PlatformStatus.ACCEPT, 12);
+        Pageable pageRequest = PageRequest.of(0, ConstantValues.PAGE_SIZE);
+
+        // when
+        Page<Platform> platform = platformRepository.findByStatus(pageRequest, PlatformStatus.ACCEPT);
+
+
+        // then
+        assertThat(platform.getContent().size()).isEqualTo(10);
+        assertThat(platform.getContent()).allSatisfy(p -> assertThat(p.getStatus()).isEqualTo(PlatformStatus.ACCEPT));
+    }
+
+
+    @Test
+    @DisplayName("플랫폼 거부 상태 조회")
+    void find_status_deny() {
+        // given
+        saveBulk(PlatformStatus.DENY, 12);
+        Pageable pageRequest = PageRequest.of(0, ConstantValues.PAGE_SIZE);
+
+        // when
+        Page<Platform> platform = platformRepository.findByStatus(pageRequest, PlatformStatus.DENY);
+
+
+        // then
+        assertThat(platform.getContent().size()).isEqualTo(10);
+        assertThat(platform.getContent()).allSatisfy(p -> assertThat(p.getStatus()).isEqualTo(PlatformStatus.DENY));
+    }
+
+
+    @Test
+    @DisplayName("플랫폼 일반 조회")
+    void find_default() {
+        // given
+        saveBulk(PlatformStatus.WAIT, 4);
+        saveBulk(PlatformStatus.ACCEPT, 4);
+        saveBulk(PlatformStatus.DENY, 4);
+        Pageable pageRequest = PageRequest.of(0, ConstantValues.PAGE_SIZE);
+
+        // when
+        Page<Platform> platform = platformRepository.findByStatus(pageRequest, null);
+
+
+        // then
+        assertThat(platform.getContent().size()).isEqualTo(10);
+    }
+
+
+
+    private void saveBulk(PlatformStatus status, Integer count) {
+        Member member = new Member(UUID.randomUUID().toString().substring(0, 5), "test", MemberRole.ROLE_USER);
+        memberRepository.save(member);
+        for (int i = 1; i <= count; ++i) {
+            Platform platform = new Platform(status.getStatus() + i , "https://tt.com", "test", member);
+            platform.changeInfo(null, status);
+            platformRepository.save(platform);
+        }
+    }
 }
 
 
