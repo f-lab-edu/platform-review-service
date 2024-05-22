9c3259f51d272fe7d609246c0ac32648b8be4fb3
penguinhing <unknown134959@naver.com>
2024-05-22 15:49:13 +0900

    홈 컨트롤러 오타 수정

--- src/main/java/com/review/rsproject/controller/HomeController.java
+++ src/main/java/com/review/rsproject/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.review.rsproject.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+
+    @GetMapping("/")
     public String home() {
         return "home";
     }
