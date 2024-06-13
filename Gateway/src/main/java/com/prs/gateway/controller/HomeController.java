dacf2cee8182b155205743a0419d828da730ee37
penguinhing <unknown134959@naver.com>
2024-06-14 04:02:35 +0900

    Gateway 설정 변경

---
+++ Gateway/src/main/java/com/prs/gateway/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.prs.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String home() {
+        return "default home";
+    }
+
+}
