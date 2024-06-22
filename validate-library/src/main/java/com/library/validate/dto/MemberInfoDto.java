fc2bd490c48d4426cc5b39d6ab7b2baa7f131d48
penguinhing <unknown134959@naver.com>
2024-06-22 23:28:32 +0900

    dto 생성자 추가

--- validate-library/src/main/java/com/library/validate/dto/MemberInfoDto.java
+++ validate-library/src/main/java/com/library/validate/dto/MemberInfoDto.java
@@ -1,9 +1,13 @@
 package com.library.validate.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberInfoDto {
 
     private Long memberId;
