68289c1202d4bbebf6502cf91e5908ee3a85dc8a
penguinhing <unknown134959@naver.com>
2024-06-29 01:54:59 +0900

    CircuitBreaker 설정

---
+++ common-library/src/main/java/com/library/common/config/ResilienceConfig.java
@@ -0,0 +1,35 @@
+package com.library.common.config;
+
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import java.time.Duration;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ResilienceConfig {
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+            .failureRateThreshold(50)
+            .waitDurationInOpenState(Duration.ofMillis(1000))
+            .slidingWindowType(SlidingWindowType.COUNT_BASED)
+            .slidingWindowSize(10)
+            .build();
+
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+            .timeoutDuration(Duration.ofSeconds(4))
+            .build();
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+            .timeLimiterConfig(timeLimiterConfig)
+            .circuitBreakerConfig(circuitBreakerConfig)
+            .build());
+    }
+
+}
