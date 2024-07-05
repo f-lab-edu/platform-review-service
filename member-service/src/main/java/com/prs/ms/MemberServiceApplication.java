27000f333519d125d5a598d92aa98819cb2092a0
penguinhing <unknown134959@naver.com>
2024-07-05 17:31:53 +0900

    분산 추적을 위한 의존성 추가

--- member-service/src/main/java/com/prs/ms/MemberServiceApplication.java
+++ member-service/src/main/java/com/prs/ms/MemberServiceApplication.java
@@ -3,7 +3,11 @@ package com.prs.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.library.common.handlers", "com.prs.ms"})
+
+@SpringBootApplication(scanBasePackages = {
+    "com.library.common.handlers",
+    "com.library.common.aop.logging",
+    "com.prs.ms"})
 public class MemberServiceApplication {
 
     public static void main(String[] args) {
